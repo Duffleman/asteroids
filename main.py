ff0a0dbdc7a9a3b7dae46c3064109cd655f2419b
Aleks <aleksander.gulla@gmail.com>
2024-11-16 15:04:07 +0100

    Added circleshape.py and player.py, updated main.py and constants.py. The player space ship is now drawn and can rotate

--- main.py
+++ main.py
@@ -3,17 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init() #Initialize all imported pygame modules
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    keep_going = True
-    while keep_going:
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0)) #Fills the screen surface with a color
+            
+        player.update(dt) #This calls the update function to move the player before each screen is drawn
+            
+        screen.fill("black") #Fills the screen surface with a color
+        player.draw(screen)
         pygame.display.flip() #Refreshes the screen
- 
+        
+        dt = clock.tick(60) / 1000 #Limit the frametime to 60 FPS
+        
+        
 if __name__ == "__main__":
     main()
